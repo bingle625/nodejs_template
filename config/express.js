ab46285b6f37795b13dd55ffcd0c1cae2bf094f6
HanSeongJae <fhqhxm8899@gmail.com>
2022-08-27 23:03:37 +0900

    ADD: cors, express, compression method-override 설정 to express file

--- config/express.js
+++ config/express.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const compression = require("compression");
+const methodOverride = require("method-override");
+const cors = require("cors");
 import testRouter from "../src/app/TestInit/TestRouter";
 
 module.exports = function () {
   const app = express();
+
+  //json 설정
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  //compression 설정
+  app.use(compression());
+
+  //method-override 설정
+  app.use(methodOverride());
+
+  //cors 설정
+  app.use(cors());
 
   app.get("/", function (req, res) {
     res.send("Hi");
